343dda042f3afba81766e220f9f9b173dc3a131f
loque18 <madridx2010@gmail.com>
2022-12-28 17:10:10 -0500

    feat: first steps

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,34 @@
+interface Rpcs {
+    chainId: string;
+    url: string;
+    explorer: string;
+}
+
+interface SmartContract {
+    key: string;
+    abi: string;
+    address: string | string[];
+}
+
+interface CelesteConfig {
+    isMultichain: boolean;
+    rpcs: {
+        [key: string]: Rpcs;
+    };
+    smartContracts: SmartContract[];
+    addressBook: {
+        [key: string]: string;
+    };
+}
+
 class CelesteJS {
-    // ...
-    public static get VERSION(): string {
-        return '0.0.111';
+    private config: CelesteConfig;
+    private provider: any;
+    readonly configRead: Readonly<CelesteConfig>;
+
+    constructor(config: CelesteConfig) {
+        console.log(config);
     }
-    // ...
 }
 
 export default CelesteJS;
