0aae8e9b47127c3c0a8b7d6d942a971fdea2adff
loque18 <madridx2010@gmail.com>
2022-12-28 12:05:07 -0500

    chore: babel and ts configured

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,1 +1,51 @@
-console.log('Hello from store');
+abstract class StateMachine {
+    abstract onEnter(): void;
+    abstract update(): void;
+    abstract onExit(): void;
+}
+
+class Iddle extends StateMachine {
+    onEnter() {
+        console.log('Iddle onEnter');
+    }
+
+    update() {
+        console.log('Taking a break');
+    }
+
+    onExit() {
+        console.log('Iddle onExit');
+    }
+}
+
+class Walk extends StateMachine {
+    onEnter() {
+        console.log('Walk onEnter');
+    }
+
+    update() {
+        console.log('Walking');
+    }
+
+    onExit() {
+        console.log('Walk onExit');
+    }
+}
+
+class Player {
+    private state: StateMachine;
+
+    constructor() {
+        this.state = new Walk();
+    }
+
+    setState(state: StateMachine) {
+        this.state.onExit();
+        this.state = state;
+        this.state.onEnter();
+    }
+
+    update() {
+        this.state.update();
+    }
+}
